510ca0cf3db4e421f58a0244d3541cc8d9cbf64e
udukunda1 <rumpatr048@gmail.com>
2024-11-07 19:01:48 +0200

    another banger

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,17 @@ import mongoose from 'mongoose';
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+  });
+
 app.use('/pharma', pharmaRoutes);
 
 
--- FrontEnd/src/pharmacy/components/pharmacy/PharmacyItemsHome.js
+++ FrontEnd/src/pharmacy/components/pharmacy/PharmacyItemsHome.js
@@ -3,10 +3,29 @@ import PharmacyItem from './PharmacyItem';
 import Button from '../../../shared/components/UI/Button/Button';
 import { pharmacies } from '../../../shared/utils/data';
 import { Link } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
 
 
 
 function PharmacyItemsHome() {
+
+    // const [resData, setResData] = useState([]);
+
+    // useEffect(() => {
+    //     async function GetData() {
+    //         try{
+    //         const data = await fetch('http://localhost:5000/pharma');
+    //         const fetched = await data.json();
+    //         setResData(fetched.pharmacies);
+    //         }
+    //         catch(err){
+    //             setResData('failed to fetch');
+    //         }
+    //     }
+    //     GetData();
+    // }, [])
+
+    // console.log(resData);
 
     const featured = pharmacies.slice(0, 3);
 
